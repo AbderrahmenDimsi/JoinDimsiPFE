dff56cd44d25ecac152a949c4ade474003f782ef
Samia BERHOUMA <s.berhouma@dimsi.fr>
2022-04-26 16:27:04 +0100

    nombre de question par module

--- Plugins/Module.cs
+++ Plugins/Module.cs
@@ -5,6 +5,7 @@ using Model.Definitions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Plugins
 {
@@ -28,40 +29,50 @@ namespace Plugins
 
                 if (context.MessageName.ToLower() == "associate" || context.MessageName.ToLower() == "disassociate")
                 {
-                    var test = 0;
                     var target = (EntityReference)context.InputParameters["Target"];
                     var relatedEntities = context.InputParameters["RelatedEntities"] as EntityReferenceCollection;
-                    var testId = relatedEntities[0].Id;
+
+                    var relatedEntityId = relatedEntities[0].Id;
                     if (target.LogicalName == "dim_test")
                     {
                         var Test = service.Retrieve(TestDefinition.EntityName, target.Id, new ColumnSet(TestDefinition.column.Module));
 
-
                         var query = new QueryExpression(QuestionDefinition.EntityName);
-                        query.ColumnSet.AddColumns(QuestionDefinition.Columns.Id, QuestionDefinition.Columns.nom, QuestionDefinition.Columns.Module);
-                        query.AddOrder(QuestionDefinition.Columns.nom, OrderType.Ascending);
-                        var queryQuestionTest = query.AddLink(QuestionDefinition.Columns.QuestionTest, QuestionDefinition.Columns.Id, QuestionDefinition.Columns.Id);
-                        var aa = queryQuestionTest.AddLink(TestDefinition.column.TestQuestion, TestDefinition.column.Id, TestDefinition.column.Id);
-                        aa.LinkCriteria.AddCondition(TestDefinition.column.Id, ConditionOperator.Equal, Test.Id);
+                        query.ColumnSet.AddColumns("dim_questionid", "dim_name", "createdon", QuestionDefinition.Columns.Module);
+                        var query_dim_dim_question_dim_test = query.AddLink("dim_dim_question_dim_test", "dim_questionid", "dim_questionid");
+                        var ab = query_dim_dim_question_dim_test.AddLink("dim_test", "dim_testid", "dim_testid");
+                        ab.LinkCriteria.AddCondition("dim_testid", ConditionOperator.Equal, Test.Id);
+
+
 
                         string ch = "";
                         var questions = service.RetrieveMultiple(query).Entities.ToList();
+                        var nums = new List<string>();
+                        List<string> nn = new List<string>();
 
-                        //lister les modules des questions 
-                        List<Entity> mod = new List<Entity>();
-                        foreach(var q in questions)
+                     
+                        foreach (var q in questions)
                         {
-                         mod[q] = q.FormattedValues[QuestionDefinition.Columns.Module]; 
+                            nums.Add(q.FormattedValues[QuestionDefinition.Columns.Module]);
+                          
                         }
-                        var questionsDistinct = questions.GroupBy(x => x.GetAttributeValue<OptionSetValue>(QuestionDefinition.Columns.Module)).Select(y => y.First()).ToList();
 
-                        foreach (var q in questionsDistinct)
+                        
+                       var m= nums.Distinct().Select(s => Tuple.Create(s, nums.Count(s2 => s2==s))).ToList();
+                 
+                        
+
+                        // var questionsDistinct = questions.GroupBy(x => x.GetAttributeValue<OptionSetValue>(QuestionDefinition.Columns.Module)).Select(y => y.First()).ToList();
+
+                        foreach (var mm in m)
                         {
                             if (questions.Count() > 1)
-                                ch = ch + " ," + q.FormattedValues[QuestionDefinition.Columns.Module];
-                            else ch = ch + q.FormattedValues[QuestionDefinition.Columns.Module];
+                                ch = ch + "  " + mm;
+                           else ch = ch + mm;
+                         
                         }
 
+                      
                         Test[TestDefinition.column.Module] = ch;
 
                         service.Update(Test);
@@ -69,7 +80,7 @@ namespace Plugins
                     if (target.LogicalName == "dim_question")
                     {
 
-                        var Test = service.Retrieve(TestDefinition.EntityName, testId, new ColumnSet(TestDefinition.column.Module));
+                        var Test = service.Retrieve(TestDefinition.EntityName, relatedEntityId, new ColumnSet(TestDefinition.column.Module));
 
 
                         var query = new QueryExpression(QuestionDefinition.EntityName);
@@ -95,16 +106,139 @@ namespace Plugins
 
                         service.Update(Test);
                     }
+                    calculNiveauDiffMoyenne(context, service);
                 }
 
 
             }
             catch (Exception ex)
             {
-
+                throw new InvalidPluginExecutionException(ex.Message);
             }
         }
+        private void calculNiveauDiffMoyenne(IPluginExecutionContext context, IOrganizationService service)
+        {
+                    var relatedEntities = context.InputParameters["RelatedEntities"] as EntityReferenceCollection;
+                    var testId = relatedEntities[0].Id;
+                    var target = (EntityReference)context.InputParameters["Target"];
+                    if (target.LogicalName == "dim_test")
+                    {
+                        var Test = service.Retrieve(TestDefinition.EntityName, target.Id, new ColumnSet(TestDefinition.column.Module));
 
+
+
+
+
+                        var query = new QueryExpression(QuestionDefinition.EntityName);
+                        query.ColumnSet.AddColumns(QuestionDefinition.Columns.Id, QuestionDefinition.Columns.nom, QuestionDefinition.Columns.Niveau_difficulte);
+                        query.AddOrder(QuestionDefinition.Columns.nom, OrderType.Ascending);
+                        var queryQuestionTest = query.AddLink(QuestionDefinition.Columns.QuestionTest, QuestionDefinition.Columns.Id, QuestionDefinition.Columns.Id);
+                        var aa = queryQuestionTest.AddLink(TestDefinition.column.TestQuestion, TestDefinition.column.Id, TestDefinition.column.Id);
+                        aa.LinkCriteria.AddCondition(TestDefinition.column.Id, ConditionOperator.Equal, Test.Id);
+
+
+
+
+
+
+                        int N = 0;
+                        double moyenne;
+                        var questions = service.RetrieveMultiple(query).Entities.ToList();
+
+
+                        foreach (var Q in questions)
+                        {
+                            if (Q.Contains(QuestionDefinition.Columns.Niveau_difficulte))
+                            {
+
+                                var niveau = Q.GetAttributeValue<OptionSetValue>(QuestionDefinition.Columns.Niveau_difficulte).Value;
+
+                                if (niveau == 914320000)
+                                    N = N + 1;
+
+                                else if (niveau == 914320001)
+                                    N = N + 2;
+
+                                else
+                                    N = N + 3;
+
+                            }
+
+                        }
+                        if (questions.Count() >= 1)
+                        {
+                            moyenne = N / questions.Count();
+                        }
+                        else
+                            moyenne = 0;
+
+
+                        Test[TestDefinition.column.Niveau_difficulte_moyenne] = moyenne;
+
+
+
+
+
+                        service.Update(Test);
+                    }
+                    if (target.LogicalName == "dim_question")
+                    {
+                        var Test = service.Retrieve(TestDefinition.EntityName, testId, new ColumnSet(TestDefinition.column.Module));
+
+                        var query = new QueryExpression(QuestionDefinition.EntityName);
+                        query.ColumnSet.AddColumns(QuestionDefinition.Columns.Id, QuestionDefinition.Columns.nom, QuestionDefinition.Columns.Niveau_difficulte);
+                        query.AddOrder(QuestionDefinition.Columns.nom, OrderType.Ascending);
+                        var queryQuestionTest = query.AddLink(QuestionDefinition.Columns.QuestionTest, QuestionDefinition.Columns.Id, QuestionDefinition.Columns.Id);
+                        var aa = queryQuestionTest.AddLink(TestDefinition.column.TestQuestion, TestDefinition.column.Id, TestDefinition.column.Id);
+                        aa.LinkCriteria.AddCondition(TestDefinition.column.Id, ConditionOperator.Equal, Test.Id);
+
+
+
+
+
+
+                        int N = 0;
+                        double moyenne;
+                        var questions = service.RetrieveMultiple(query).Entities.ToList();
+
+
+                        foreach (var Q in questions)
+                        {
+                            if (Q.Contains(QuestionDefinition.Columns.Niveau_difficulte))
+                            {
+
+                                var niveau = Q.GetAttributeValue<OptionSetValue>(QuestionDefinition.Columns.Niveau_difficulte).Value;
+
+                                if (niveau == 914320000)
+                                    N = N + 1;
+
+                                else if (niveau == 914320001)
+                                    N = N + 2;
+
+                                else
+                                    N = N + 3;
+
+                            }
+
+                        }
+                        if (questions.Count() >= 1)
+                        {
+                            moyenne = N / questions.Count();
+                        }
+                        else
+                            moyenne = 0;
+
+
+                        Test[TestDefinition.column.Niveau_difficulte_moyenne] = moyenne;
+
+
+
+
+
+                        service.Update(Test);
+
+                    }
+                }
 
     }
 
